d2d7599073036431d63ae6b4e8ff3461b236f7cf
moddingg33k <moddingg33k@t-online.de>
2014-07-26 04:09:57 +0200

    ZRAM: switched to num_devices=2
    Signed-off-by: moddingg33k <moddingg33k@t-online.de>

--- drivers/staging/zram/zram_drv.c
+++ drivers/staging/zram/zram_drv.c
@@ -47,7 +47,7 @@ static struct zram *zram_devices;
 #define ALLOC_ERROR_LOG_RATE_MS 1000
 
 /* Module params (documentation at end) */
-static unsigned int num_devices = 1;
+static unsigned int num_devices = 2;
 
 #define ZRAM_ATTR_RO(name)						\
 static ssize_t zram_attr_##name##_show(struct device *d,		\
